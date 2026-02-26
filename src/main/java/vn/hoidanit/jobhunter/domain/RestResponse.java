e65fcd94091de4c42625a8af9b83a4e3cc9a69ef
phamloc2362005 <phamloc2362005@gmail.com>
2026-02-26 21:19:59 +0700

    format response before sending

---
+++ src/main/java/vn/hoidanit/jobhunter/domain/RestResponse.java
@@ -0,0 +1,46 @@
+package vn.hoidanit.jobhunter.domain;
+
+public class RestResponse<T> {
+    private int status;
+    private String error;
+    private Object message;
+    private T data;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    @Override
+    public String toString() {
+        return "RestResponse [status=" + status + ", error=" + error + ", message=" + message + ", data=" + data + "]";
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public Object getMessage() {
+        return message;
+    }
+
+    public void setMessage(Object message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+}

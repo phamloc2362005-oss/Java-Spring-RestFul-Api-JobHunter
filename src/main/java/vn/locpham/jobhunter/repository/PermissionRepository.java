a57cd1e3d3c090bb0e16d80d97fca82dc0c09b6a
phamloc2362005 <phamloc2362005@gmail.com>
2026-03-21 10:20:40 +0700

    CRUD Role

--- src/main/java/vn/locpham/jobhunter/repository/PermissionRepository.java
+++ src/main/java/vn/locpham/jobhunter/repository/PermissionRepository.java
@@ -1,5 +1,7 @@
 package vn.locpham.jobhunter.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -7,10 +9,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import vn.locpham.jobhunter.domain.Permission;
+import vn.locpham.jobhunter.domain.Skill;
 
 public interface PermissionRepository
         extends JpaRepository<Permission, Long>, JpaSpecificationExecutor<Permission> {
     boolean existsByModuleAndApiPathAndMethod(String module, String apiPath, String method);
 
     Page<Permission> findAll(Specification<Permission> spec, Pageable pageable);
+
+    List<Permission> findByIdIn(List<Long> id);
 }
